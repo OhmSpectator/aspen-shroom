c85ae5d23bc62867ab227d5c74913695b2893e39
Nikolay Martyanov <nmartyanov@ptsecurity.com>
2017-04-11 15:52:43 +0300

    WIP

--- show_mode.c
+++ show_mode.c
@@ -10,23 +10,42 @@
 #define SVC_MODE 0b10011
 
 #define SCR_NS_BIT_OFFSET 0
-#define SCR_NS_BIT (1 << SCR_NS_BIT_OFFSET)
+#define SCR_NS_BIT (1U << SCR_NS_BIT_OFFSET)
 
 #define SCR_HCE_BIT_OFFSET 8
-#define SCR_HCE_BIT (1 << SCR_HCE_BIT_OFFSET)
+#define SCR_HCE_BIT (1U << SCR_HCE_BIT_OFFSET)
 
 #define SCR_SCD_BIT_OFFSET 7
-#define SCR_SCD_BIT (1 << SCR_SCD_BIT_OFFSET)
+#define SCR_SCD_BIT (1U << SCR_SCD_BIT_OFFSET)
+
+#define SCTLR_C_BIT_OFFSET 2
+#define SCTLR_C_BIT (1U << SCTLR_C_BIT_OFFSET)
 
 
 int main(int argc, char* const argv[])
 {
 
     init_print();
-    print_magic();
-    init_magic();
 
     print_magic();
+    init_magic();
+    print_magic();
+
+    printf("Disable data caches...\n");
+    (void) getc();
+    volatile u_int32_t sctlr_val;
+    u_int32_t sctlr_c_bit = ~SCTLR_C_BIT;
+    asm volatile(
+        "mrc p15, 0, r0, c1, c0, 0\n"
+        "ldr r1, %0\n"
+        "and r0, r1\n"
+        "mcr p15, 0, r0, c1, c0, 0\n"
+        "isb"
+        :
+        : "m" (sctlr_c_bit)
+        : "r0", "r1"
+    );
+    printf("Done!\n");
 
     printf("Press any button to read ID_PFR1\n");
     (void) getc();
@@ -39,11 +58,9 @@ int main(int argc, char* const argv[])
         : "r0"
     );
     printf("ID_PFR1: 0x%08" PRIx32 "\n", id_pfr1_val);
-    printf("Done!\n");
 
     printf("Press any button to read SCTLR\n");
     (void) getc();
-    uint32_t sctlr_val;
     asm volatile(
         "mrc p15, 0, r0, c1, c0, 0\n"
         "str r0, %0"
@@ -51,8 +68,7 @@ int main(int argc, char* const argv[])
         :
         : "r0"
     );
-    printf("SCTLR: 0x08%" PRIx32 "\n", sctlr_val);
-    printf("Done!\n");
+    printf("SCTLR: 0x08%" PRIx32 " (@0x%08" PRIx32 ")\n", sctlr_val, (u_int32_t)&sctlr_val);
 
     printf("Press any button to read CPSR\n");
     (void) getc();
@@ -65,7 +81,6 @@ int main(int argc, char* const argv[])
         : "r0"
     );
     printf("CPSR: 0x%08" PRIx32 "\n", cpsr_val);
-    printf("Done!\n");
 
     printf("Press any button to read SCR\n");
     (void) getc();
@@ -78,7 +93,6 @@ int main(int argc, char* const argv[])
         : "r0"
     );
     printf("SCR: 0x%08" PRIx32 "\n", scr_val);
-    printf("Done!\n");
 
     printf("Press any button to switch to MON mode (via CPS)\n");
     (void) getc();
@@ -91,7 +105,7 @@ int main(int argc, char* const argv[])
         "mov lr, r2"
         :
         : "I" (MON_MODE)
-        : "r1", "r2", "cc", "memory"
+        : "r1", "r2", "r3", "cc", "memory"
     );
     printf("Done!\n");
 
@@ -105,7 +119,6 @@ int main(int argc, char* const argv[])
         : "r0"
     );
     printf("CPSR: 0x%08" PRIx32 "\n", cpsr_val);
-    printf("Done!\n");
 
     printf("Press any button to set SCR.NS to 1\n");
     (void) getc();
@@ -130,7 +143,6 @@ int main(int argc, char* const argv[])
         : "r0"
     );
     printf("SCR: 0x%08" PRIx32 "\n", scr_val);
-    printf("Done!\n");
 
     printf("Press any button to read HVBAR\n");
     (void) getc();
@@ -143,7 +155,6 @@ int main(int argc, char* const argv[])
         : "r0"
     );
     printf("HVBAR: 0x%08" PRIx32 "\n", hvbar_val);
-    printf("Done!\n");
 
     printf("Press any button to setup HVBAR\n");
     (void) getc();
@@ -168,7 +179,6 @@ int main(int argc, char* const argv[])
         : "r0"
     );
     printf("HVBAR: 0x%08" PRIx32 "\n", hvbar_val);
-    printf("Done!\n");
 
     printf("Press any button to set SCR.HCE and SRC.SCD to 1\n");
     (void) getc();
@@ -193,31 +203,124 @@ int main(int argc, char* const argv[])
         : "r0"
     );
     printf("SCR: 0x%08" PRIx32 "\n", scr_val);
-    printf("Done!\n");
 
-    u_int32_t local_magic = 0xABADC0DE;
-    printf("Loacl magic: 0x%08X\n", local_magic);
+    printf("Press \'s\' to exit, any other button to continue\n");
+    if(getc() == 's')
+        return 0;
 
-    printf("Press any button to switch to SVC mode (via CPS)\n");
+    printf("Press any button to read TTBCR\n");
+    (void)getc();
+    u_int32_t ttbcr_val;
+    asm volatile(
+        "mrc p15, 0, r0, c2, c0, 2\n"
+        "str r0, %0"
+        : "=m" (ttbcr_val)
+        :
+        : "r0"
+    );
+    printf("TTBCR: 0x%08" PRIx32 "\n", ttbcr_val);
+
+    printf("Press any button to read TTBR0\n");
+    (void)getc();
+    u_int32_t ttbr0_low_val, ttbr0_high_val;
+    asm volatile(
+        "mrrc p15, 0, r0, r1, c2\n"
+        "str r0, %0\n"
+        "str r1, %1\n"
+        : "=m" (ttbr0_low_val), "=m" (ttbr0_high_val)
+        :
+        : "r0", "r1"
+    );
+    printf("TTBR0: low - 0x%08" PRIx32 ", high - 0x%08" PRIx32 "\n", ttbr0_low_val, ttbr0_high_val);
+
+    volatile u_int32_t local_magic = 0xABADC0DE;
+    printf("Local magic: 0x%08X\n", local_magic);
+
+    printf("Press any button to switch to non-secure SVC mode (via CPS)\n");
     (void) getc();
     asm volatile(
+        "dsb\n"
+        "dmb\n"
         "mov r0, sp\n"
-        "mov r1, lr\n"
-        "mrs r2, spsr\n"
+        //"mov r1, lr\n"
+        //"mrs r2, spsr\n"
+        //"mov r3, fp\n"
         "cps %0\n"
-        "msr spsr, r2\n"
+        //"msr spsr, r2\n"
         "isb\n"
         "mov sp, r0\n"
-        "mov lr, r1\n"
+        //"mov lr, r1\n"
+        //"mov fp, r3\n"
         "dsb"
         :
         : "I" (SVC_MODE)
-        : "r0", "r1", "sp", "cc", "memory"
+        : "r0", "r1", "r2", "r3", "sp", "cc", "memory"
     );
     init_print();
     printf("Done!\n");
     print_magic();
-    printf("Loacl magic: 0x%08X\n", local_magic);
+    printf("Local magic: 0x%08X\n", local_magic);
+
+    printf("Press any button to read SCTLR\n");
+    (void) getc();
+    u_int32_t sctlr_ns_val;
+    asm volatile(
+        "mrc p15, 0, r0, c1, c0, 0\n"
+        "str r0, %0"
+        : "=m" (sctlr_ns_val)
+        :
+        : "r0"
+    );
+    printf("SCTLR: 0x08%" PRIx32 "\n", sctlr_ns_val);
+
+    printf("Press any button to fix SCTLR\n");
+    printf("Trying to put 0x%08" PRIx32 " from @0x%08" PRIx32 "!\n", sctlr_val, (u_int32_t)&sctlr_val);
+    (void) getc();
+    asm volatile(
+        "ldr r0, %0\n"
+        "mcr p15, 0, r0, c1, c0, 0\n"
+        "dsb\n"
+        "isb\n"
+        :
+        : "m" (sctlr_val)
+        : "r0"
+    );
+    printf("Done!\n");
+
+    printf("Press any button to read SCTLR\n");
+    sctlr_ns_val = 241;
+    (void) getc();
+        asm volatile(
+        "mrc p15, 0, r0, c1, c0, 0\n"
+        "str r0, %0"
+        : "=m" (sctlr_ns_val)
+        :
+        : "r0"
+    );
+    printf("SCTLR: 0x08%" PRIx32 "\n", sctlr_ns_val);
+
+    printf("Press any button to read TTBCR\n");
+    (void)getc();
+    asm volatile(
+        "mrc p15, 0, r0, c2, c0, 2\n"
+        "str r0, %0"
+        : "=m" (ttbcr_val)
+        :
+        : "r0"
+    );
+    printf("TTBCR: 0x%08" PRIx32 "\n", ttbcr_val);
+
+    printf("Press any button to read TTBR0\n");
+    (void)getc();
+    asm volatile(
+        "mrrc p15, 0, r0, r1, c2\n"
+        "str r0, %0\n"
+        "str r1, %1\n"
+        : "=m" (ttbr0_low_val), "=m" (ttbr0_high_val)
+        :
+        : "r0", "r1"
+    );
+    printf("TTBR0: low - 0x%08" PRIx32 ", high - 0x%08" PRIx32 "\n", ttbr0_low_val, ttbr0_high_val);
 
     printf("Press any button to read CPSR\n");
     (void) getc();
@@ -229,11 +332,10 @@ int main(int argc, char* const argv[])
         : "r0"
     );
     printf("CPSR: 0x%08" PRIx32 "\n", cpsr_val);
-    printf("Done!\n");
 
     printf("Press \'s\' to exit, any other button to continue\n");
     if(getc() == 's')
-        return 0;
+        return 241;
 
     printf("Press any button to enter HYP mode (via hvc)\n");
     (void) getc();
@@ -257,8 +359,33 @@ int main(int argc, char* const argv[])
         :
         : "r0"
     );
-    printf("Done!\n");
     printf("CPSR: 0x%08" PRIx32 "\n", cpsr_val);
+
+    printf("Press any button to read HSCTLR\n");
+    (void) getc();
+    uint32_t hsctlr_val;
+    asm volatile(
+        "mrc p15, 0, r0, c1, c0, 0\n"
+        "str r0, %0"
+        : "=m" (hsctlr_val)
+        :
+        : "r0"
+    );
+    printf("HSCTLR: 0x08%" PRIx32 "\n", hsctlr_val);
+
+    printf("Press any button to read HTTBR\n");
+    (void)getc();
+    u_int32_t httbr_low_val, httbr_high_val;
+    asm volatile(
+        "mrrc p15, 4, r0, r1, c2\n"
+        "str r0, %0\n"
+        "str r1, %1\n"
+        : "=m" (httbr_low_val), "=m" (httbr_high_val)
+        :
+        : "r0", "r1"
+    );
+    printf("HTTBR: low - 0x%08" PRIx32 ", high - 0x%08" PRIx32 "\n", httbr_low_val, httbr_high_val);
+
     (void) getc();
 
     return 0;
